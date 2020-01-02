04bb269a4d1420769e05a5af9ca1c69b07c684a1
Darian Sampare <dsampare04@gmail.com>
2020-01-02 12:54:28 -0800

    env variable for db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const applications = require("./routes/api/applications");
 
@@ -7,7 +8,8 @@ const app = express();
 app.use(express.json());
 
 // DB Congig
-const db = require("./config/keys").mongoURI;
+if (process.env.NODE_ENV === " production") process.env.mongoURI;
+else const db = require("./config/keys").mongoURI;
 
 // Connect to Mongo
 mongoose
@@ -17,6 +19,16 @@ mongoose
 
 // Use Routes
 app.use("/api/applications", applications);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === " production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));

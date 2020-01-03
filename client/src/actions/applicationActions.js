7e9da22835f7853a082763fb09ecf77e3598ff9c
Darian Sampare <dsampare04@gmail.com>
2020-01-03 13:40:03 -0800

    authentication, login and logout

--- client/src/actions/applicationActions.js
+++ client/src/actions/applicationActions.js
@@ -1,37 +1,54 @@
-import axios from "axios";
+import axios from 'axios';
 import {
   GET_APPLICATIONS,
   ADD_APPLICATION,
   DELETE_APPLICATION,
   APPLICATIONS_LOADING
-} from "./types";
+} from './types';
+import { tokenConfig } from './authActions';
+import { returnErrors } from './errorActions';
 
 export const getApplications = () => dispatch => {
   dispatch(setApplicationsLoading());
-  axios.get("/api/applications").then(res =>
-    dispatch({
-      type: GET_APPLICATIONS,
-      payload: res.data
-    })
-  );
+  axios
+    .get('/api/applications')
+    .then(res =>
+      dispatch({
+        type: GET_APPLICATIONS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
-export const deleteApplication = id => dispatch => {
-  axios.delete(`/api/applications/${id}`).then(res =>
-    dispatch({
-      type: DELETE_APPLICATION,
-      payload: id
-    })
-  );
+export const deleteApplication = id => (dispatch, getState) => {
+  axios
+    .delete(`/api/applications/${id}`, tokenConfig(getState))
+    .then(res =>
+      dispatch({
+        type: DELETE_APPLICATION,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
-export const addApplication = application => dispatch => {
-  axios.post("/api/applications", application).then(res =>
-    dispatch({
-      type: ADD_APPLICATION,
-      payload: res.data
-    })
-  );
+export const addApplication = application => (dispatch, getState) => {
+  axios
+    .post('/api/applications', application, tokenConfig(getState))
+    .then(res =>
+      dispatch({
+        type: ADD_APPLICATION,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 export const setApplicationsLoading = () => {
